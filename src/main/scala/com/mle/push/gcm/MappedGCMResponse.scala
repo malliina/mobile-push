dd334d4fdadc88e3e463856c84e5a536cd207d59
malliina <malliina123@gmail.com>
2015-03-15 20:37:38 +0200

    0.9.1

--- src/main/scala/com/mle/push/gcm/MappedGCMResponse.scala
+++ src/main/scala/com/mle/push/gcm/MappedGCMResponse.scala
@@ -1,12 +1,27 @@
 package com.mle.push.gcm
 
+import com.mle.push.gcm.MappedGCMResponse.TokenReplacement
 import play.api.libs.json.Json
 
 /**
  * @author Michael
  */
-case class MappedGCMResponse(ids: Seq[String], response: GCMResponse)
+case class MappedGCMResponse(ids: Seq[String], response: GCMResponse) {
+  lazy val replacements: Seq[TokenReplacement] = {
+    if (response.canonical_ids > 0) {
+      for {
+        (id, result) <- ids zip response.results
+        canonical <- result.registration_id.toSeq
+      } yield TokenReplacement(id, canonical)
+    } else {
+      Nil
+    }
+  }
+}
 
 object MappedGCMResponse {
   implicit val json = Json.reads[MappedGCMResponse]
+
+  case class TokenReplacement(oldToken: String, newToken: String)
+
 }
