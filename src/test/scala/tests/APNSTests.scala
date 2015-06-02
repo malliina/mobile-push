8d5c286e13f7b72f3d7fa81515e50c16104f2621
malliina <malliina123@gmail.com>
2015-06-02 21:35:58 +0300

    Refactor tests

--- src/test/scala/tests/APNSTests.scala
+++ src/test/scala/tests/APNSTests.scala
@@ -19,36 +19,41 @@ import scala.util.Try
  * @author Michael
  */
 class APNSTests extends FunSuite {
-  val deviceID = "9f3c2f830256954ada78bf56894fa7586307f0eedb7763117c84e0c1eee8347a"
+  //  val deviceID = Some("9f3c2f830256954ada78bf56894fa7586307f0eedb7763117c84e0c1eee8347a")
+  val deviceID: Option[String] = None
 
   test("certificate is valid") {
     val creds = APNSCreds.load
     KeyStores.validateKeyStore(creds.file, creds.pass, "PKCS12")
   }
 
-  test("send notification with body") {
-    val creds = APNSCreds.load
-    val ks = keyStoreFromFile(creds.file, creds.pass, "PKCS12").get
-    val client = new APNSClient(ks, creds.pass, isSandbox = true)
-    //    val message = APNSMessage.badged("I <3 U!", 3)
-    val payload = AlertPayload(
-      "this is a body",
-      title = Some("hey"),
-      actionLocKey = Some("POMP"),
-      locKey = Some("MSG_FORMAT"),
-      locArgs = Some(Seq("Emilia", "Jaana")))
-    val advancedMessage = APNSMessage(APSPayload(Some(Right(payload)), sound = Some("default")))
-    val fut = client.push(deviceID, advancedMessage)
-    Await.result(fut, 5.seconds)
+  test("send notification with body, if enabled") {
+    deviceID.foreach(token => {
+      val creds = APNSCreds.load
+      val ks = keyStoreFromFile(creds.file, creds.pass, "PKCS12").get
+      val client = new APNSClient(ks, creds.pass, isSandbox = true)
+      //    val message = APNSMessage.badged("I <3 U!", 3)
+      val payload = AlertPayload(
+        "this is a body",
+        title = Some("hey"),
+        actionLocKey = Some("POMP"),
+        locKey = Some("MSG_FORMAT"),
+        locArgs = Some(Seq("Emilia", "Jaana")))
+      val advancedMessage = APNSMessage(APSPayload(Some(Right(payload)), sound = Some("default")))
+      val fut = client.push(token, advancedMessage)
+      Await.result(fut, 5.seconds)
+    })
   }
 
-  test("send background notification") {
-    val creds = APNSCreds.load
-    val ks = keyStoreFromFile(creds.file, creds.pass, "PKCS12").get
-    val client = new APNSClient(ks, creds.pass, isSandbox = true)
-    val message = APNSMessage.background(badge = 15)
-    println(Json.prettyPrint(Json.toJson(message)))
-    Await.result(client.push(deviceID, message), 5.seconds)
+  test("send background notification, if enabled") {
+    deviceID.foreach(token => {
+      val creds = APNSCreds.load
+      val ks = keyStoreFromFile(creds.file, creds.pass, "PKCS12").get
+      val client = new APNSClient(ks, creds.pass, isSandbox = true)
+      val message = APNSMessage.background(badge = 15)
+      println(Json.prettyPrint(Json.toJson(message)))
+      Await.result(client.push(token, message), 5.seconds)
+    })
   }
 
   def keyStoreFromFile(file: Path, pass: String, storeType: String = "JKS"): Try[KeyStore] = Try {
--- src/test/scala/tests/GCMTests.scala
+++ src/test/scala/tests/GCMTests.scala
@@ -1,6 +1,6 @@
 package tests
 
-import com.mle.push.gcm.{MappedGCMResponse, GCMClient, GCMMessage, GCMResponse}
+import com.mle.push.gcm.{GCMClient, GCMMessage, MappedGCMResponse}
 import org.scalatest.FunSuite
 
 import scala.concurrent.duration.DurationInt
@@ -10,17 +10,20 @@ import scala.concurrent.{Await, Future}
  * @author Michael
  */
 class GCMTests extends FunSuite {
-  test("send message") {
-    //    val gcmApiKey: String = "AIzaSyCCDniLRhlHAfnXIJnsVn-You2QQKLfrM8"
-    val gcmApiKey: String = "AIzaSyBLwdU7XGCdEPlwkGXW7V2eMRRFieNGYmA"
-    val emuID4 = "APA91bHyeY6NdQar-XXoC47PuWB0eCZErLB-xBNSlhrXQ-u_ElWM7ZFaocsoCeWBx_Or5vmj357BNTdr6atRNwAfFQ4od458OqwfJV3SSPnYa1CIN1j0EVplN8QeEjx3n6-WV6obKN60CDn0-RL3gAsILC_4ec0gAQ"
-    val pushIDs = Seq(emuID4)
+  //  val emuID = Some("APA91bHyeY6NdQar-XXoC47PuWB0eCZErLB-xBNSlhrXQ-u_ElWM7ZFaocsoCeWBx_Or5vmj357BNTdr6atRNwAfFQ4od458OqwfJV3SSPnYa1CIN1j0EVplN8QeEjx3n6-WV6obKN60CDn0-RL3gAsILC_4ec0gAQ")
+  val emuID: Option[String] = None
+  test("send message, if enabled") {
+    emuID.foreach(token => {
+      //    val gcmApiKey: String = "AIzaSyCCDniLRhlHAfnXIJnsVn-You2QQKLfrM8"
+      val gcmApiKey: String = "AIzaSyBLwdU7XGCdEPlwkGXW7V2eMRRFieNGYmA"
+      val pushIDs = Seq(token)
 
-    val client = new GCMClient(gcmApiKey)
-    val message = GCMMessage(Map("title" -> "hey you", "message" -> "late night sexy åäö", "key" -> "value"))
-    val response: Future[Seq[MappedGCMResponse]] = client.pushAll(pushIDs, message)
-    val rs = Await.result(response, 5.seconds)
-    assert(rs.forall(r => r.response.failure === 0))
-    rs.foreach(println)
+      val client = new GCMClient(gcmApiKey)
+      val message = GCMMessage(Map("title" -> "hey you", "message" -> "late night sexy åäö", "key" -> "value"))
+      val response: Future[Seq[MappedGCMResponse]] = client.pushAll(pushIDs, message)
+      val rs = Await.result(response, 5.seconds)
+      assert(rs.forall(r => r.response.failure === 0))
+      rs.foreach(println)
+    })
   }
 }
