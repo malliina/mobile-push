051e54d156a453e55c4b55464d8850f6f5af5706
Matt Oliver <mjo2089@gmail.com>
2021-01-23 09:25:35 -0500

    Change implicit ordering

--- src/main/scala/com/malliina/push/gcm/gcmModels.scala
+++ src/main/scala/com/malliina/push/gcm/gcmModels.scala
@@ -24,6 +24,25 @@ object GCMResponse {
   implicit val json = Json.reads[GCMResponse]
 }
 
+case class GCMNotification(
+  title: Option[String],
+  body: Option[String],
+  subtitle: Option[String],
+  sound: Option[String],
+  badge: Option[String],
+  icon: Option[String],
+  click_action: Option[String],
+  body_loc_key: Option[String],
+  title_loc_key: Option[String],
+  android_channel_id: Option[String],
+  tag: Option[String],
+  color: Option[String]
+)
+
+object GCMNotification {
+  implicit val json = Json.format[GCMNotification]
+}
+
 case class GCMLetter(
   registration_ids: Seq[GCMToken],
   data: Map[String, String],
@@ -58,23 +77,4 @@ object GCMMessage {
   implicit val json = Json.format[GCMMessage]
 
   type FCMMessage = GCMMessage
-}
-
-case class GCMNotification(
-  title: Option[String],
-  body: Option[String],
-  subtitle: Option[String],
-  sound: Option[String],
-  badge: Option[String],
-  icon: Option[String],
-  click_action: Option[String],
-  body_loc_key: Option[String],
-  title_loc_key: Option[String],
-  android_channel_id: Option[String],
-  tag: Option[String],
-  color: Option[String]
-)
-
-object GCMNotification {
-  implicit val json = Json.format[GCMNotification]
 }
